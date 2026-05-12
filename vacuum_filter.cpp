a72d1196a32f7b54071acd54fa3e174500123ce2
Martina Dadić <md54715@fer.hr>
2026-05-13 00:25:40 +0200

    dodan lookup, delete i dio inserta

--- vacuum_filter.cpp
+++ vacuum_filter.cpp
@@ -8,26 +8,24 @@ using namespace std;
 class VacuumFilter {
     public:
         int noOfBuckets; //m
-        uint16_t buckets[100][4]; //tablica koja sadrži m buckets od kojih svaka ima 4 polja u kojima se spremaju fingerprints
-        int L[4]; //polje koje sadrzi duljine AR-ova
+        uint16_t buckets[1000][4] = {}; //tablica koja sadrži m buckets od kojih svaka ima 4 polja u kojima se spremaju fingerprints
+        int L[4] = {}; //polje koje sadrzi duljine AR-ova
         int n; //ukupan broj itema
 
-        uint16_t b1(uint64_t x){ //1. kandidat
-            return a5hash( &x, sizeof( x ), 0 ) % noOfBuckets;
-        }
-        uint16_t b2(uint64_t x){
-            return Alt(b1(x), x%16);
+        VacuumFilter(int m){ //konstruktor
+            noOfBuckets = m;
+            n=0;
         }
         uint16_t Alt(uint16_t b, uint16_t f){
-            if (n < pow(2, 18)){
+            if (n < 262144){ //2^18
                 return AltManji(b, f);
             }
             return AltVeci(b, f);
         }
         uint16_t AltVeci(uint16_t b, uint16_t f){
-            if (L==NULL){
-                for (int i=0;i<4;i++){
-                    L[i]=RangeSelection(n, 0.95, (1 - i/4));
+            if (L[0]==0){
+                for (int i=1;i<5;i++){
+                    L[i-1]=RangeSelection(n, 0.95, (1.0 - i / 4.0));
                 }
                 L[3]*=2; //povecamo zadnji da izbjegnemo fail
             }
@@ -48,7 +46,7 @@ class VacuumFilter {
             return L;
         }
         bool LoadFactorTest(int n, float alpha, int r, int L){
-            int m = ceil(n/(4*alpha*L)*L);//broj bucket-a
+            int m = ceil((double)n / (4.0 * alpha * L) * L); //broj bucket-a
             int N = 4*r*m*alpha; //broj unesenih elemenata
             int c=m/L; //broj chunk-ova
             float P = 0.97 * 4 * L;
@@ -58,22 +56,66 @@ class VacuumFilter {
             return false;
         }
         float EstimatedMaxLoad(int N, int c){
-            return n/c + 3/2*sqrt((2*n/c)*log(c));
+            return N/c + 1.5*sqrt((2*N/c)*log(c));
         }
-        bool lookup(){
+        bool insert(int x){
+            uint16_t hashX = a5hash(&x, sizeof(x), 0); //hash itema
+            uint16_t f=hashX & 0xFFFF; //fingerprint item-a
+            uint16_t b1 = hashX % noOfBuckets; //1. kandidat
+            uint16_t b2 = Alt(b1, f); //2. kandidat
+            for(int i=0;i<4;i++){
+                if (buckets[b1][i]==0){
+                    buckets[b1][i]=f;
+                    n++;
+                    return true;
+                }
+                if (buckets[b2][i]==0){
+                    buckets[b2][i]=f;
+                    n++;
+                    return true;
+                }
+            }
+            int randomNumber = rand() % 8; //random broj od 0 do 7
             return false;
         }
-        bool insert(){
+        bool lookup(int x){
+            uint16_t hashX = a5hash(&x, sizeof(x), 0); //hash itema
+            uint16_t f=hashX & 0xFFFF; //fingerprint item-a
+            uint16_t b1 = hashX % noOfBuckets; //1. kandidat
+            uint16_t b2 = Alt(b1, f); //2. kandidat
+            for(int i=0;i<4;i++){
+                if (buckets[b1][i]==f){
+                    return true;
+                }
+                if (buckets[b2][i]==f){
+                    return true;
+                }
+            }
             return false;
         }
-        bool remove(){
+        bool remove(int x){
+            uint16_t hashX = a5hash(&x, sizeof(x), 0); //hash itema
+            uint16_t f=hashX & 0xFFFF; //fingerprint item-a
+            uint16_t b1 = hashX % noOfBuckets; //1. kandidat
+            uint16_t b2 = Alt(b1, f); //2. kandidat
+            for(int i=0;i<4;i++){
+                if (buckets[b1][i]==f){
+                    buckets[b1][i]=0;
+                    return true;
+                }
+                if (buckets[b2][i]==f){
+                    buckets[b2][i]=0;
+                    return true;
+                }
+            }
             return false;
         }
         
 };
 
 int main(){
-    VacuumFilter filter;
-    filter.noOfBuckets = 5;
+    VacuumFilter filter(5000);
     cout << filter.noOfBuckets;
+    cout << "Load Factor Test za n=1000, alpha=0.95, r=10 i L=20: " << filter.LoadFactorTest(1000, 0.95, 10, 20) <<"\n";
+    cout << "Range selection za n=1000, alpha=0.95, r=10: " << filter.RangeSelection(1000, 0.95, 10) <<"\n";
 }
