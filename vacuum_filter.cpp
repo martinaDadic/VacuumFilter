d53cf5879d5e9a7d5d54c1a096f2a008a3ccc659
Martina Dadić <md54715@fer.hr>
2026-05-12 13:58:57 +0200

    dodana alt funkcija

--- vacuum_filter.cpp
+++ vacuum_filter.cpp
@@ -2,12 +2,15 @@
 #include "a5hash.h"
 #include <cstdint>
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 class VacuumFilter {
     public:
         int noOfBuckets; //m
         uint16_t buckets[100][4]; //tablica koja sadrži m buckets od kojih svaka ima 4 polja u kojima se spremaju fingerprints
+        int L[4]; //polje koje sadrzi duljine AR-ova
+        int n; //ukupan broj itema
 
         uint16_t b1(uint64_t x){ //1. kandidat
             return a5hash( &x, sizeof( x ), 0 ) % noOfBuckets;
@@ -16,7 +19,30 @@ class VacuumFilter {
             return Alt(b1(x), x%16);
         }
         uint16_t Alt(uint16_t b, uint16_t f){
-            return b ^ a5hash(&f, sizeof(f), 0);
+            if (n < pow(2, 18)){
+                return AltManji(b, f);
+            }
+            return AltVeci(b, f);
+        }
+        uint16_t AltVeci(uint16_t b, uint16_t f){
+            if (L==NULL){
+                for (int i=0;i<4;i++){
+                    L[i]=RangeSelection(n, 0.95, (1 - i/4));
+                }
+                L[3]*=2; //povecamo zadnji da izbjegnemo fail
+            }
+            int l = L[f % 4]; //trenutni AR
+            uint16_t delta = a5hash(&f, sizeof(f), 0) % l;
+            return b ^ delta;
+        }
+        uint16_t AltManji(uint16_t b, uint16_t f){
+            uint16_t delta = a5hash(&f, sizeof(f), 0) % noOfBuckets;
+            uint16_t b_2=(b-delta) % noOfBuckets;
+            b_2=(noOfBuckets - 1 - b_2 + delta) % noOfBuckets;
+            return b_2;
+        }
+        int RangeSelection(int n, float alpha, int r){
+            return 0;
         }
         bool lookup(){
             return false;
