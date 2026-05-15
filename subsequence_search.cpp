283287d01ec90637975413092a78409ff72028f9
Frane Bazo <frane.bazo@fer.hr>
2026-05-15 18:20:44 +0200

    Postavljanje osnovne strukture

---
+++ subsequence_search.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <set>
+#include <algorithm>
+#include <random>
+#include <filesystem>
+
+using namespace std;
+
+mt19937 rng(42);
+
+int main() {
+    return 0;
+}
