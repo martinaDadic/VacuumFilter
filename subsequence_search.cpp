a9cf914cf6cdd58cd376e19a247c68dc1505c1ec
Frane Bazo <frane.bazo@fer.hr>
2026-05-15 18:34:56 +0200

    Funkcije za stvaranje i spremanje umjetnog genoma

--- subsequence_search.cpp
+++ subsequence_search.cpp
@@ -11,6 +11,35 @@ using namespace std;
 
 mt19937 rng(42);
 
+string generate_sequence(int size) {
+    string base = "AGCT";
+    string sequence;
+    sequence.reserve(size);
+
+    uniform_int_distribution<int> dist(0, 3);
+
+    for (int i = 0; i < size; i++) {
+        sequence += base[dist(rng)];
+    }
+
+    return sequence;
+}
+
+void save_fasta(const string& filename, const string& name, const string& sequence) {
+    ofstream file(filename);
+
+    file << ">" << name << "\n";
+
+    for (int i = 0; i < (int)sequence.size(); i += 60) {
+        file << sequence.substr(i, 60) << "\n";
+    }
+}
+
 int main() {
+    string artificial_filename = "sekvenca.fasta";
+
+    string artificial_sequence = generate_sequence(1000000);
+    save_fasta(artificial_filename, "Umjetno generiran genom", artificial_sequence);
+
     return 0;
 }
